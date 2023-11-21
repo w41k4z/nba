9111847ef7257eca7cdd02e38117103ad6db5fc1
w41k4z <alainricor@gmail.com>
2023-11-21 11:09:12 +0300

    first commit

---
+++ nba-web-service/src/test/java/proj/tp/nba/NbaApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.tp.nba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
