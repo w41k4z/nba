90f23dd807a3b7c2195982e8577fac218e32f9a1
w41k4z <alainricor@gmail.com>
2023-11-21 13:35:12 +0300

    Stat front end OK, waiting for web service

--- nba-stat/src/components/StatTable.jsx
+++ nba-stat/src/components/StatTable.jsx
@@ -3,6 +3,28 @@ import React from 'react'
 import images from '../image'
 
 const StatTable = () => {
+  const [stat, setStat] = React.useState([])
+  React.useEffect(() => { 
+    setStat([
+      {
+        classement: 1,
+        joueur: 'Joel Embiid',
+        joueur_image: 'joel_embiid.png',
+        equipe: 'PHI',
+        m: 13,
+        mj: 13,
+        ppm: 31.9,
+        rpm: 11.2,
+        pdpm: 6.2,
+        mpm: 34.1,
+        eff: 36.4,
+        fg: 50.2,
+        p3: 31.8,
+        lf: 88.5
+      }
+    ])
+  }, [])
+
   return (
       <div className='table-responsive'>
           <table className="table table-striped">
@@ -23,28 +45,32 @@ const StatTable = () => {
                     <th scope="col">LF%</th>
                   </tr>
               </thead>
-              <tbody className="px-2">
+            <tbody className="px-2">
+              {stat.map((item, index) => {
+                return (
                   <tr>
-                    <td>1</td>
-                      <td>
-                          <div className='d-flex justify-content-center align-items-center'>
-                <img src={images['joel_embiid.png']} className='img-fluid me-2' height='35px' width='35px' alt='Player Joel Embiid' />
-                <span className='ms-2'>Joel Embiid</span>
-                          </div>
+                    <td>{item.classement}</td>
+                    <td>
+                      <div className='d-flex justify-content-center align-items-center'>
+                        <img src={images[item.joueur_image]} className='img-fluid me-2' height='35px' width='35px' alt={'Player ' + item.joueur} />
+                        <span className='ms-2'>{item.joueur}</span>
+                      </div>
                     </td>
-                    <td>Equipe 1</td>
-                    <td>13</td>
-                    <td>13</td>
-                    <td>31.9</td>
-                    <td>11.2</td>
-                    <td>6.2</td>
-                    <td>34.1</td>
-                    <td>36.4</td>
-                    <td>50.2</td>
-                    <td>31.8</td>
-                    <td>88.5</td>
+                    <td>{item.equipe}</td>
+                    <td>{item.m}</td>
+                    <td>{item.mj}</td>
+                    <td>{item.ppm}</td>
+                    <td>{item.rpm}</td>
+                    <td>{item.pdpm}</td>
+                    <td>{item.mpm}</td>
+                    <td>{item.eff}</td>
+                    <td>{item.fg}</td>
+                    <td>{item.p3}</td>
+                    <td>{item.lf}</td>
                   </tr>
-              </tbody>
+                )
+              })}
+            </tbody>
           </table>
     </div>
   )
